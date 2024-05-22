5151cae38a2c49d53a1ec6b9d0dfdd820b161029
Ganesh Shejwal <ganesh.shejwal@bnt.local>
2024-05-22 20:17:41 +0530

    commit changes

--- src/main/java/com/webapplication/employeeProject/EmployeeProjectApplication.java
+++ src/main/java/com/webapplication/employeeProject/EmployeeProjectApplication.java
@@ -8,7 +8,6 @@ public class EmployeeProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeProjectApplication.class, args);
-		System.out.println("prgramme running");
 	}
 
 }
